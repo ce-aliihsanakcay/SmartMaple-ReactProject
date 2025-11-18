3a49103758ea486b38bf00ae70a26234e9e34d84
aliihsanakcay <ali.akcay@donetr.com>
2025-11-18 21:52:09 +0300

    scss fixes

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -122,11 +122,13 @@ const CalendarContainer = ({ schedule, auth }: CalendarContainerProps) => {
         date: assignmentDate,
         staffId: filteredAssignments[i]?.staffId,
         shiftId: filteredAssignments[i]?.shiftId,
-        className: `event ${
-          getAssigmentById(filteredAssignments[i]?.id)?.isUpdated
+        className: `event 
+          ${getAssigmentById(filteredAssignments[i]?.id)?.isUpdated
             ? "highlight"
             : ""
-        } ${!isValidDate ? "invalid-date" : ""}`,
+          } 
+          ${!isValidDate ? "invalid-date" : ""} 
+        `,
       };
       works.push(work);
     }
@@ -172,13 +174,13 @@ const CalendarContainer = ({ schedule, auth }: CalendarContainerProps) => {
     <div className="calendar-section">
       <div className="calendar-wrapper">
         <div className="staff-list">
-          {schedule?.staffs?.map((staff: any) => (
+          {schedule?.staffs?.map((staff: any, index: number) => (
             <div
               key={staff.id}
               onClick={() => setSelectedStaffId(staff.id)}
-              className={`staff ${
-                staff.id === selectedStaffId ? "active" : ""
-              }`}
+              className={`staff color-${index} borderColor-${index}
+                ${staff.id === selectedStaffId ? `active color-white bg-${index} fill-white` : ""} 
+              `}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -186,12 +188,19 @@ const CalendarContainer = ({ schedule, auth }: CalendarContainerProps) => {
                 viewBox="0 -960 960 960"
                 width="20px"
               >
-                <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17-62.5t47-43.5q60-30 124.5-46T480-440q67 0 131.5 16T736-378q30 15 47 43.5t17 62.5v112H160Zm320-400q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm160 228v92h80v-32q0-11-5-20t-15-14q-14-8-29.5-14.5T640-332Zm-240-21v53h160v-53q-20-4-40-5.5t-40-1.5q-20 0-40 1.5t-40 5.5ZM240-240h80v-92q-15 5-30.5 11.5T260-306q-10 5-15 14t-5 20v32Zm400 0H320h320ZM480-640Z" />
+                <path
+                  className={`
+                    fill-${index} 
+                    ${staff.id === selectedStaffId ? `active fill-white` : ""}
+                  `}
+                  d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17-62.5t47-43.5q60-30 124.5-46T480-440q67 0 131.5 16T736-378q30 15 47 43.5t17 62.5v112H160Zm320-400q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm160 228v92h80v-32q0-11-5-20t-15-14q-14-8-29.5-14.5T640-332Zm-240-21v53h160v-53q-20-4-40-5.5t-40-1.5q-20 0-40 1.5t-40 5.5ZM240-240h80v-92q-15 5-30.5 11.5T260-306q-10 5-15 14t-5 20v32Zm400 0H320h320ZM480-640Z"
+                />
               </svg>
               <span>{staff.name}</span>
             </div>
           ))}
         </div>
+
         <FullCalendar
           ref={calendarRef}
           locale={auth.language}
@@ -255,9 +264,10 @@ const CalendarContainer = ({ schedule, auth }: CalendarContainerProps) => {
 
             return (
               <div
-                className={`${found ? "" : "date-range-disabled"} ${
-                  isHighlighted ? "highlighted-date-orange" : ""
-                } highlightedPair`}
+                className={`${found ? "" : "date-range-disabled"} 
+                  ${isHighlighted ? "highlighted-date-orange" : ""} 
+                  highlightedPair 
+                `}
               >
                 {dayjs(date).date()}
               </div>
